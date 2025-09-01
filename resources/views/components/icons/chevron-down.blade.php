732598c32c8b786ef41ba2d7afc8275d6c08bcb2
Neon2905 <tpshine1234@gmail.com>
2025-09-01 13:57:54 +0630

    added: main component for recipe

---
+++ resources/views/components/icons/chevron-down.blade.php
@@ -0,0 +1,4 @@
+<svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10.2916 4.875L6.49998 8.66667L2.70831 4.875" stroke="#3F3F46" stroke-width="2" stroke-linecap="round"
+        stroke-linejoin="round" />
+</svg>
--- resources/views/recipe.blade.php
+++ resources/views/recipe.blade.php
@@ -1,4 +1,47 @@
 @php
+    $recipe = (object) [
+        'title' => 'Lemon-Garlic Chicken',
+        'author' => (object) [
+            'name' => 'Katie Lee Biegel',
+            'profile_url' => '/images/profile-icons/katile-lee-biegel.png',
+        ],
+        'date' => 'Nov 29, 2023',
+        'rating' => 4,
+        'reviews_count' => 132,
+        'image' =>
+            'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2022/12/19/KC3213-katie-lee-biegel-lemon-garlic-chicken_s4x3.jpg.rend.hgtvcom.826.620.suffix/1671496800903.webp',
+        'total_time' => 125,
+        'prep_time' => 25,
+        'level' => 'Easy',
+        'cuisine' => 'Malaysian',
+        'ingredients_count' => 14,
+        'meal' => 'Lunch',
+        'yield' => 4,
+        'reviews' => [
+            (object) [
+                'name' => 'Alice',
+                'review' => 'Loved this recipe! Super creamy.',
+                'profile_url' => '/images/profile-icons/alice.png',
+                'date' => '08/07/2024',
+                'rating' => 4,
+            ],
+            (object) [
+                'name' => 'Someone',
+                'review' => 'It’s very tasty when I added mushroom.',
+                'profile_url' => '/images/profile-icons/someone.png',
+                'date' => '20/06/2024',
+                'rating' => 5,
+            ],
+            (object) [
+                'name' => 'Bob',
+                'review' => 'Added shrimp — turned out amazing.',
+                'profile_url' => '/images/profile-icons/bob.png',
+                'date' => '12/12/2023',
+                'rating' => 4,
+            ],
+        ],
+    ];
+
     $directions = [
         'Prepare' =>
             'In a small bowl, combine the olive oil, mustard, garlic powder, paprika, minced garlic, lemon zest and juice and 1 teaspoon salt.',
@@ -15,33 +58,33 @@
 @endphp
 
 <x-app class="gap-5">
-    <div class="modal-card px-6 flex-1 h-500 gap-7">
+    <div class="modal-card px-6 flex-1 gap-7">
         {{-- header --}}
         <div class="flex-center flex-col gap-1">
-            <h1 class="">Lemon-Garlic Chicken</h1>
+            <h1 class="">{{ $recipe->title }}</h1>
             <div class="flex-center flex-col">
-                <h3>RECIPE BY KATILE LEE BIEGEL</h3>
-                <h4 class="text-muted">Nov 29, 2023</h4>
+                <h3>RECIPE BY {{ $recipe->author->name }}</h3>
+                <h4 class="text-muted">{{ $recipe->date }}</h4>
             </div>
             <div class="flex-center flex-col mt-2 gap-1">
-                <x-rating :value="4" />
-                <h4 class="text-muted">132 Reviews</h4>
+                <x-rating :value="$recipe->rating" />
+                <h4 class="text-muted">{{ $recipe->reviews_count }} Reviews</h4>
             </div>
         </div>
         {{-- media --}}
-        <img src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2022/12/19/KC3213-katie-lee-biegel-lemon-garlic-chicken_s4x3.jpg.rend.hgtvcom.826.620.suffix/1671496800903.webp"
-            alt="hero-image">
+        <img src="{{ $recipe->image }}" alt="hero-image">
         {{-- summary --}}
         <div class="flex-center border border-navbar-gray h-30 p-2">
             <div class="flex flex-center flex-1 h-full">
                 <div class="flex-center flex-col items-start h-25 gap-3 text-left">
                     <h4 class="w-full">
                         <span class="font-normal">Ready In:</span>
-                        2 hr 5 mins
+                        {{-- TODO:abstract this. --}}
+                        {{ floor($recipe->total_time / 60) > 0 ? floor($recipe->total_time / 60) . ' hr ' : '' }}{{ $recipe->total_time % 60 > 0 ? $recipe->total_time % 60 . ' mins' : '' }}
                     </h4>
                     <h4 class="w-full">
                         <span class="font-normal">Prep Time:</span>
-                        25 mins
+                        {{ $recipe->prep_time }} mins
                     </h4>
                 </div>
             </div>
@@ -53,7 +96,7 @@
                     </h4>
                     <h4 class="w-full">
                         <span class="font-normal">Ingredients:</span>
-                        14
+                        {{ $recipe->ingredients_count }}
                     </h4>
                 </div>
             </div>
@@ -61,27 +104,27 @@
                 <div class="flex-center flex-col items-start h-25 gap-3 text-left">
                     <h4 class="w-full">
                         <span class="font-normal">Cuisine:</span>
-                        Malaysian
+                        {{ $recipe->cuisine }}
                     </h4>
                     <h4 class="w-full">
                         <span class="font-normal">Meal:</span>
-                        Lunch
+                        {{ $recipe->meal }}
                     </h4>
                     <h4 class="w-full">
                         <span class="font-normal">Yield:</span>
-                        4 servings
+                        {{ $recipe->yield }} servings
                     </h4>
                 </div>
             </div>
         </div>
         {{-- description --}}
         <div class="px-3 w-full">
-            <h2 class="text-heading-lg font-highlight font-medium">I love dishes that can be made in advance for a
+            <h3 class="text-heading-md font-highlight font-medium">I love dishes that can be made in advance for a
                 dinner party. This
                 chicken is totally simple. You marinate
                 it in the same dish you cook it in, then pop it in the oven about 45 minutes before you want to eat.
                 It’s so
-                easy and flavorful—a real crowd pleaser!</h2>
+                easy and flavorful—a real crowd pleaser!</>
         </div>
         {{-- directions --}}
         <div class="flex flex-col text-left w-full p-2 gap-2">
@@ -89,18 +132,18 @@
             <ol class="list-decimal list-inside space-y-2 px-2 text-body-lg font-semibold">
                 @foreach ($directions as $title => $step)
                     <li>
-                        {{ $title }}: <span class=" font-medium">{{ $step }}</span>
+                        {{ $title }}: <span class="font-medium">{{ $step }}</span>
                     </li>
                 @endforeach
             </ol>
         </div>
-        {{-- aurthor --}}
+        {{-- author --}}
         <div class="flex items-center justify-between rounded-xl w-full bg-gray px-6 py-3">
             <div class="flex gap-4">
-                <img class="size-23" src="/images/profile-icons/katile-lee-biegel.png" alt="profile">
+                <img class="size-23" src="{{ $recipe->author->profile_url }}" alt="profile">
                 <div class="flex justify-center flex-col text-left">
-                    <h2 class="text-heading-lg">By Katile Lee Biegel</h2>
-                    <h5 class="font-normal">Food Blogger & Recipe Creator</h5>
+                    <h2 class="text-heading-lg">By {{ $recipe->author->name }}</h2>
+                    <h5 class="font-normal">{{ $recipe->author->name }}</h5>
                 </div>
             </div>
             <button class="flex-center button h-10 rounded-full bg-light-gray w-auto px-4">
@@ -109,10 +152,62 @@
             </button>
         </div>
         {{-- reviews --}}
-        <div class="flex flex-col items-start rounded-xl w-full bg-gray px-6 py-3">
+        <div class="flex flex-col items-start rounded-xl w-full bg-gray px-6 py-7 gap-5">
             <div class="flex flex-col gap-1 items-start">
-                <h2 class="text-heading-lg">132 Reviews</h2>
-                <x-rating />
+                <h2 class="text-heading-lg">{{ $recipe->reviews_count }} Reviews</h2>
+                <x-rating value=4 />
+            </div>
+            <form class="flex-center flex-col space-between gap-2 w-full p-3" method="POST" action="#">
+                @csrf
+                <div class="flex justify-start gap-2 w-full">
+                    <img class="rounded rounded-full size-13" src="/images/profile-icons/me.png" alt="">
+                    <textarea
+                        class="flex-1 rounded-lg px-4 py-2 text-body-lg bg-background focus:outline-none focus:ring-2 focus:ring-primary resize-y min-h-20"
+                        placeholder="Did you make this recipe? Leave a review!"></textarea>
+                </div>
+                <div class="flex justify-between space-between w-full">
+                    <div class="flex items-center gap-2 text-subtitle-lg font-semibold">
+                        Your Rating:
+                        <x-rating />
+                    </div>
+                    <button
+                        class="flex-center justify-between button bg-tertiary text-black text-subtitle-md font-semibold rounded-full">
+                        Post Review
+                    </button>
+                </div>
+            </form>
+            <div class="flex-center w-full flex-col gap-3">
+                <div
+                    class="flex justify-end items-center w-full border-b border-on-background text-subtitle-md gap-1 pb-2">
+                    Sort by
+                    <x-icons.chevron-down class="ml-2" />
+                </div>
+                @foreach ($recipe->reviews as $review)
+                    <div
+                        class="flex flex-col justify-start items-start px-2 pb-2 w-full gap-1 border-b border-on-background">
+                        {{-- Header --}}
+                        <div class="flex gap-3">
+                            <img class="size-10" src="{{ $review->profile_url }}" alt="{{ $review->name }}">
+                            <div class="flex flex-col">
+                                <h3>
+                                    {{ $review->name }} <span class="text-subtitle-md font-normal text-muted">
+                                        {{-- TODO: fix later --}}
+                                        {{ \Carbon\Carbon::createFromFormat('d/m/Y', $review->date)->diffForHumans() }}
+                                    </span>
+                                </h3>
+                                {{-- TODO:Fix sizing issue on rating component [can't use with under 4] --}}
+                                <x-rating class="w-20" size='4' value="{{ $review->rating }}" />
+                            </div>
+                        </div>
+                        {{-- Review --}}
+                        <h3 class="font-highlight font-normal">
+                            {{ $review->review }}
+                        </h3>
+                    </div>
+                @endforeach
+                <button class="button rounded-full text-body-md font-normal bg-tertiary">
+                    Show more reviews
+                </button>
             </div>
         </div>
     </div>
