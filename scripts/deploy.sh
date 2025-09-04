e00010a5770cd3c9d5b79437e29159b533d38f45
Neon <tpshine1234@gmail.com>
2025-09-04 16:19:02 +0630

    Add Docker configuration files, Nginx setup, and deployment script for production

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Starting Laravel deployment script..."
+
+echo "installing npm packages"
+npm install
+
+echo "Running npm build script..."
+npm run build
+
+echo "Running composer"
+composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Caching views..."
+php artisan view:cache
+
+echo "Clearing old caches..."
+php artisan cache:clear
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Deployment complete."
